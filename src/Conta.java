36e8a61fc3cd2ce3a85102f9530baf40e5bc829a
Mateus Melo <mateusmelo207@gmail.com>
2026-04-13 23:23:41 -0300

    Sistema bancário com menu e POO

--- src/Conta.java
+++ src/Conta.java
@@ -1,2 +1,17 @@
-public class Conta {
-}
+    public class Conta {
+        double saldo;
+
+        public void verSaldo() {
+            System.out.println("Saldo: " + saldo);
+        }
+        public void depositar(double valor) {
+            saldo += valor;
+        }
+        public void sacar(double valor) {
+            if (valor > saldo) {
+                System.out.println("Saldo Insuficiente");
+            } else  {
+                saldo -= valor;
+            }
+        }
+    }
--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,39 @@
 import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            Scanner scanner = new Scanner(System.in);
-            double saldo = 0;
-            int opcao;
+        Scanner scanner = new Scanner(System.in);
+        Conta conta = new Conta();
 
-            do {
-                System.out.println("\n=== BANCO ===");
-                System.out.println("1 - Ver saldo");
-                System.out.println("2 - Depositar");
-                System.out.println("3 - Sacar");
-                System.out.println("0 - Sair");
-                System.out.print("Escolha: ");
+        int opcao;
 
-                opcao = scanner.nextInt();
+        do {
+            System.out.println("\n=== BANCO ===");
+            System.out.println("1 - Ver saldo");
+            System.out.println("2 - Depositar");
+            System.out.println("3 - Sacar");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha: ");
 
-                if (opcao == 1) {
-                    System.out.println("Saldo: R$ " + saldo);
+            opcao = scanner.nextInt();
 
-                } else if (opcao == 2) {
-                    System.out.print("Valor para depositar: ");
-                    double deposito = scanner.nextDouble();
-                    saldo += deposito;
+            if (opcao == 1) {
+                conta.verSaldo();
 
-                } else if (opcao == 3) {
-                    System.out.print("Valor para sacar: ");
-                    double saque;
-                    saque = scanner.nextDouble();
+            } else if (opcao == 2) {
+                System.out.print("Valor para depositar: ");
+                double valor = scanner.nextDouble();
+                conta.depositar(valor);
 
-                    if (saque > saldo) {
-                        System.out.println("Saldo insuficiente!");
-                    } else {
-                        saldo -= saque;
-                    }
-                }
+            } else if (opcao == 3) {
+                System.out.print("Valor para sacar: ");
+                double valor = scanner.nextDouble();
+                conta.sacar(valor);
+            }
 
-            } while (opcao != 0);
+        } while (opcao != 0);
 
-            System.out.println("Programa encerrado.");
-        }
+        System.out.println("Programa encerrado.");
     }
+}
